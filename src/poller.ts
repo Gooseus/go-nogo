9be673b67318cd202d604b87147dfe48b376911e
Shawn Marincas <shawn@allstar.gg>
2024-10-12 19:53:45 -0400

    fix: cleaning up?

--- src/poller.ts
+++ src/poller.ts
@@ -36,21 +36,21 @@ export class Poller {
 
   private evaluateResponse(response: Response): 'success' | 'fail' | 'poll' {
     const { expectedResponses } = this.options;
-    const { statusCode, body } = response;
+    const { statusCode, body: responseBody } = response;
 
     if (expectedResponses?.length > 0) {
-      for (const { action, bodyRegex, body: expectedBody, status } of expectedResponses) {
-        const statusMatch = !status || status === statusCode;
-        const bodyMatch = !expectedBody || expectedBody === body;
-        const bodyRegexMatch = !bodyRegex || bodyRegex.test(body);
+      for (const { action, bodyRegex, body: expectedBody, status: expectedStatus } of expectedResponses) {
+        const statusMatch = !expectedStatus || expectedStatus === statusCode;
+        const bodyMatch = !expectedBody || expectedBody === responseBody;
+        const bodyRegexMatch = !bodyRegex || bodyRegex.test(responseBody);
 
         if (statusMatch && (bodyMatch || bodyRegexMatch)) return action || 'success';
       }
     } else {
       // No expected responses
       if (statusCode === this.options.expectStatus) {
-        if (this.options?.expectBody !== body) return 'poll';
-        if (this.options.expectBodyRegex?.test(body)) return 'poll';
+        if (this.options?.expectBody && this.options?.expectBody !== responseBody) return 'poll';
+        if (this.options?.expectBodyRegex && this.options?.expectBodyRegex?.test(responseBody)) return 'poll';
         return 'success';
       }
     }
