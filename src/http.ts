1106b8ddcb9f210be6a09613ee9b814179a96262
Shawn Marincas <shawn@allstar.gg>
2024-10-12 21:07:48 -0400

    fix: make it work

--- src/http.ts
+++ src/http.ts
@@ -11,6 +11,7 @@ export class HttpClient implements Client {
   async request(method: string, url: string): Promise<Response> {
     console.log(`Requesting ${method} ${url} in env ${process.env.NODE_ENV}`);
     if (url.includes('localhost')) url = url.replace('localhost', '127.0.0.1');
+    if (url.includes('::1')) url = url.replace('::1', '127.0.0.1');
 
     const response = await this.client.request(method, url, null, {});
     const statusCode = response.message.statusCode || 0;
